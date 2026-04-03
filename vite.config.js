a1bef326752a27ac367bedf7331f87736f5d36ee
jdeinnocenti <jdeinnocenti@gmail.com>
2026-04-03 17:57:16 -0300

    fix: vercel build - devDependencies y config correctos

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Super Campeones',
+        short_name: 'SC',
+        description: 'Pronosticá el Mundial 2026 y competí con amigos',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#080c14',
+        theme_color: '#c9a84c',
+        icons: [
+          { src: '/icons/icon-192.png', sizes: '192x192', type: 'image/png', purpose: 'any' },
+          { src: '/icons/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts',
+              expiration: { maxEntries: 10, maxAgeSeconds: 31536000 }
+            }
+          }
+        ]
+      }
+    })
+  ]
 })
